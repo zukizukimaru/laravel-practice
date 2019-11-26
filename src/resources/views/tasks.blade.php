0ed6bec7ff78255cddd36071539aeec49b30fd68
email <kazucchi.98323@gmail.com>
2019-11-26 15:21:47 +0900

    サーバ上にタスクの欄を作った

---
+++ src/resources/views/tasks.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<form action="{{url('task')}}" method="post" class="form-horisontal">
+  @csrf
+<div class="form-group">
+    <label for="task" class="col-sm-3 control-label">タスク</label>
+    <div class="col-sm-6">
+        <input type="text" name="task" id="task-name" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <div class="col-sm-offset-3 col-sm-6">
+            <button type="submit" class="btn btn-default">
+                <i class="fa fa-plus"></i>タスク
+            </button>
+        </div>
+    </div>
+</div>
+</form>
+@endsection
